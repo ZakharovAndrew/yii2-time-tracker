025627a722c62147ee0fa92d9942f9f906c28348
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2024-06-26 08:24:51 +0300

    Add files via upload

---
+++ m240111_145911_create_time_tracking_role_activity_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `time_tracking_role_activity`.
+ */
+class m240111_145911_create_time_tracking_activity_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable(
+            'time_tracking_role_activity',
+            [
+                'id' => $this->primaryKey(),
+                'role_id' => $this->integer(),
+                'activity_id' => $this->integer(),
+                'pos' => $this->integer()->defaultValue(0)
+            ]
+        );
+    }
+
+    public function down()
+    {
+        $this->dropTable('time_tracking_role_activity');
+    }
+}
