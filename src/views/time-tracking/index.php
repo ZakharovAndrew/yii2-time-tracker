80e118b304ffbd9bce1534f1b1f3a25448ef7979
Zakharov Andrey <prostonado_07@mail.ru>
2024-06-28 17:10:06 +0300

    fixed view

--- src/views/time-tracking/index.php
+++ src/views/time-tracking/index.php
@@ -12,6 +12,8 @@ use yii\helpers\Url;
 
 $this->title = Module::t('Time Tracking');
 $this->params['breadcrumbs'][] = $this->title;
+
+$last_activity = end($user_activity);
 ?>
 <link
     rel="stylesheet"
@@ -35,18 +37,25 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?php if (Yii::$app->getModule('timetracker')->showTitle) {?><h1><?= Html::encode($this->title) ?></h1><?php } ?>
 
-    <?php if (is_array($user_activity) && count($user_activity) == 0) {?>
+    <?php if (!$user_activity || (is_array($user_activity) && count($user_activity) == 0)) {?>
     <p>
         <?= Html::a(Module::t('Start the working day'), ['start'], ['class' => 'btn btn-success']) ?>
     </p>
-    <?php } else { ?>
+    <?php } else if ($last_activity->activity_id != Activity::STOP_DAY) { ?>
     <p>
         <?= Html::a('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
   <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
 </svg> '.Module::t('Add Activity'), ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a(Module::t('Finish the working day'), ['stop'], ['class' => 'btn btn-danger']) ?>
     </p>
+    <?php } ?>
     
-    <div class="time-tracking-box animate__animated animate__fadeInUp">
+    <?php if ($last_activity !== false && $last_activity->activity_id == Activity::STOP_DAY) { ?>
+    <div class="alert alert-info"><?= Module::t('You have completed your working day.') ?></div>
+    <?php } ?>
+    
+    <?php if ($user_activity) { ?>
+    <div class="time-tracking-box animate__animated animate__faster animate__fadeInUp">
         <table class="table">
             <thead>
                 <tr>
