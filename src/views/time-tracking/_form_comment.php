99c3d5284b64dff8cae5554cbc657bc9dfabd386
Zakharov Andrey <prostonado_07@mail.ru>
2024-07-11 02:58:19 +0300

    added comment editing in activity

---
+++ src/views/time-tracking/_form_comment.php
@@ -0,0 +1,24 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use ZakharovAndrew\TimeTracker\Module;
+
+/** @var yii\web\View $this */
+/** @var ZakharovAndrew\TimeTracker\models\TimeTracking $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="time-tracking-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'comment')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Module::t('Save'), ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ src/views/time-tracking/edit-comment.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use ZakharovAndrew\TimeTracker\Module;
+
+/** @var yii\web\View $this */
+/** @var ZakharovAndrew\TimeTracker\models\TimeTracking $model */
+
+$this->title = Module::t('Edit comment');
+$this->params['breadcrumbs'][] = ['label' => Module::t('Time Tracking'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="time-tracking-create">
+
+    <?php if (Yii::$app->getModule('timetracker')->showTitle) {?><h1><?= Html::encode($this->title) ?></h1><?php } ?>
+
+    <?= $this->render('_form_comment', [
+        'model' => $model,
+    ]) ?>
+
+</div>
