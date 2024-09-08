81d316f83fd32201294b4bb81be7af9eda6a13c1
Zakharov Andrey <prostonado_07@mail.ru>
2024-09-08 15:03:12 +0300

    fixed and added Break

--- src/models/Activity.php
+++ src/models/Activity.php
@@ -16,6 +16,7 @@ class Activity extends \yii\db\ActiveRecord
 {
     const START_DAY = 1000000;
     const STOP_DAY  = 1000001;
+    const BREAK_DAY = 1000002;
     
     /**
      * {@inheritdoc}
@@ -55,35 +56,33 @@ class Activity extends \yii\db\ActiveRecord
         return ArrayHelper::map(static::find()->asArray()->all(), 'id', 'name');
     }
     
-    static public function getActivityByUserId($user_id)
+    static public function userActivity($user_id, $additionalCondition = null)
     {
-        $list = static::find()
-                ->where('id in (select activity_id as id FROM time_tracking_role_activity a WHERE a.role_id in (SELECT role_id FROM user_roles WHERE user_id = '.$user_id.' ))')
-                ->asArray()
-                ->all();
+        $query = static::find()
+                ->where('id in (select activity_id as id FROM time_tracking_role_activity a WHERE a.role_id in (SELECT role_id FROM user_roles WHERE user_id = '.(int)$user_id.' ))');
         
-        return ArrayHelper::map($list, 'id', 'name');
+        if ($additionalCondition !== null) {
+            $query->andWhere($additionalCondition);
+        }
+
+        return $query->asArray()->all();
     }
     
+    static public function getActivityByUserId($user_id)
+    {        
+        return ArrayHelper::map(static::userActivity($user_id), 'id', 'name');
+    }
+    
+    /**
+     * Get hints for activities available to the user
+     */
     static public function getHintsActivityByUserId($user_id)
     {
-        $list = static::find()
-                ->where('id in (select activity_id as id FROM time_tracking_role_activity a WHERE a.role_id in (SELECT role_id FROM user_roles WHERE user_id = '.$user_id.' ))')
-                ->andWhere('hint <> \'\' AND hint IS NOT NULL')
-                ->asArray()
-                ->all();
-        
-        return ArrayHelper::map($list, 'id', 'hint');
+        return ArrayHelper::map(static::userActivity($user_id, "hint <> '' AND hint IS NOT NULL"), 'id', 'hint');
     }
     static public function getTemplateCommentsActivityByUserId($user_id)
     {
-        $list = static::find()
-                ->where('id in (select activity_id as id FROM time_tracking_role_activity a WHERE a.role_id in (SELECT role_id FROM user_roles WHERE user_id = '.$user_id.' ))')
-                ->andWhere('comment_templates <> \'\' AND comment_templates IS NOT NULL')
-                ->asArray()
-                ->all();
-        
-        return ArrayHelper::map($list, 'id', 'comment_templates');
+        return ArrayHelper::map(static::userActivity($user_id, "comment_templates <> '' AND comment_templates IS NOT NULL"), 'id', 'comment_templates');
     }
     
     static public function getList()
@@ -92,6 +91,7 @@ class Activity extends \yii\db\ActiveRecord
         
         $list[static::START_DAY] = Module::t('The begining of the work day');
         $list[static::STOP_DAY] = Module::t('End of the working day');
+        $list[static::BREAK_DAY] = Module::t('Break');
         
         return $list;
     }
