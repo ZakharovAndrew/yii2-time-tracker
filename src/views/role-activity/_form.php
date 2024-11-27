c483edc54e0b81e1a21337f755f956e925121963
Zakharov Andrey <prostonado_07@mail.ru>
2024-11-28 01:27:35 +0300

    fixed the template

--- src/views/role-activity/_form.php
+++ src/views/role-activity/_form.php
@@ -3,6 +3,9 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use ZakharovAndrew\TimeTracker\Module;
+use ZakharovAndrew\user\assets\UserAssets;
+
+UserAssets::register($this);
 
 /** @var yii\web\View $this */
 /** @var app\models\RoleActivity $model */
@@ -11,11 +14,13 @@ use ZakharovAndrew\TimeTracker\Module;
 
 <div class="role-activity-form">
 
+    
     <?php $form = ActiveForm::begin(); ?>
-
+        <div class="white-block">
     <?= $form->field($model, 'activity_id')->dropDownList(\ZakharovAndrew\TimeTracker\models\Activity::getDropdownList(), ['prompt'=>'', 'class' => 'form-control form-select']) ?>
 
     <?= $form->field($model, 'pos')->textInput() ?>
+        </div>
 
     <div class="form-group">
         <?= Html::submitButton(Module::t('Save'), ['class' => 'btn btn-success']) ?>
