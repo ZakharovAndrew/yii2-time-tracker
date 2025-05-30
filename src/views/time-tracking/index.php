60fcd958f2bba5b48e1d548169854db944318c70
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2025-05-30 11:51:58 +0300

    Update index.php

--- src/views/time-tracking/index.php
+++ src/views/time-tracking/index.php
@@ -199,7 +199,7 @@ $this->registerJs($script, yii\web\View::POS_READY);
                                 ?>
                             </td>
                             <?php foreach ($additionalProperties as $prop) { ?>
-                            <td><?= $prop->getUserPropertyValue($item->activity_id) ?></td>
+                            <td><?= $prop->getUserPropertyValue($item->id) ?></td>
                             <?php } ?>
                         </tr>
                         <?php } ?>
